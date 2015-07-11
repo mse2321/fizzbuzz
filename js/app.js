9834fb631521572205f1c59acbed6082e55c61e8
mse2321 <mse233@gmail.com>
2015-07-11 12:20:16 -0400

    finished fizzbuzz

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready( function() {
+	
+	//Display Numbers Handler
+	$("#number-display").html(numberGenerator);
+
+	function numberGenerator() {
+		for (n = 1; n <= 100; n++) {
+
+			if (n % 3 == 0 && n % 5 == 0) {
+			$("#number-display").append("FizzBuzz" + "<br />");
+			} else if (n % 5 == 0 && n % 3 != 0) {
+				$("#number-display").append("Buzz" + "<br />");
+			} else if (n % 3 == 0 && n % 5 != 0) {
+				$("#number-display").append("Fizz" + "<br />");
+			} else {
+			$("#number-display").append(n + "<br />");
+			}
+		}
+	}
+});
